2d45c0d2632db3580c5933c88243d8bc3fc00c3e
SaracensFan <matthewclifford@hotmail.co.uk>
2021-03-30 09:37:06 +0100

    update including js file

---
+++ js/main.js
@@ -0,0 +1,8 @@
+function navHead() {
+    let nav = document.getElementById("navbar");
+    if(nav.ClassName = "navigate") {
+        nav.className += " responsive";
+    } else {
+        nav.className = "navigate"
+    }
+}
