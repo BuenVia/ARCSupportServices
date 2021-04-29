e7e71702353636c988eb64f8a2d990ce7edd406d
Matt Clifford <matthewclifford@hotmail.co.uk>
2021-04-29 14:01:15 +0100

    admin pages

---
+++ admin/header.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>ARC Admin</title>
+    <link rel="stylesheet" href="../css/resetstyle.css">
+    <link rel="stylesheet" href="../css/admin.css">
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css2?family=Play&display=swap" rel="stylesheet">
+    
+    <script src="https://kit.fontawesome.com/ac3ba74ae6.js" crossorigin="anonymous"></script>
+</head>
+<body>
+    
+<p>Header</p>
---
+++ admin/index.php
@@ -0,0 +1,10 @@
+<?php
+    require 'header.php';
+?>
+
+<p>Main body</p>
+
+
+<?php
+    require 'footer.php'
+?>
