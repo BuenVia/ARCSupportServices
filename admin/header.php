755ce56ce42e55dd1b3dee2488bfebeea834899e
SaracensFan <matthewclifford@hotmail.co.uk>
2021-04-29 14:03:01 +0100

    update from laptop

--- admin/header.php
+++ admin/header.php
@@ -14,4 +14,25 @@
 </head>
 <body>
     
-<p>Header</p>
+<div class="container">
+
+    <div class="row">
+        <div class="column-33" style="text-align:centre">
+            <a href="/admin/index.php"><img src="/design/images/new_arc_logo.png" style="height:80px"></a>
+        </div>
+
+        <div class="column-66" style="text-align:right">
+        <?php
+            if (isset($_SESSION['user'])) {
+
+                echo '<p style="color:white">You are logged in as <a style="color:white"><b><u>'.$_SESSION['user'].'</u></b></a></p>
+                        <a href="/admin/includes/logoutad.inc.php"><button class="btn7 lnk"><b><u>Logout</b></u></button></a></p>';
+            }
+            else {
+                echo '<p style="color:white"><b>You are <u>LOGGED OUT</u></b></p>';
+            }
+        ?>
+        <p style="color:white"><b>Admin Area</b></p>
+    </div>
+
+</div>
