7c6a66ccaf39cdbfa8b83e07bb565de6cec5f303
Matt Clifford <matthewclifford@hotmail.co.uk>
2021-04-30 09:22:13 +0100

    changes

--- admin/admin.php
+++ admin/admin.php
@@ -4,10 +4,22 @@
 
 <main>
 
-<a href="create.php"><button>Create Bodyshop</button></a><br>
-<a href="create.php"><button>Create User</button></a><br>
-<a href="create.php"><button>Create Admin</button></a><br>
+<div class="fluid-container">
+    <div class="row">
+
+        <div class="col-sm">
+            <a href="create.php"><button>Create Bodyshop</button></a><br>
+            <a href="create.php"><button>Create User</button></a><br>
+            <a href="create.php"><button>Create Admin</button></a><br>
+        </div>
     
+    <div class="col-sm"></div>
+    
+    <div class="col-sm"></div>
+    
+    </div>
+</div>
+   
 
 </main>
 
