e9ccf0b3dc27b4be236fabc67db95e2c6c2f33eb
Matt Clifford <matthewclifford@hotmail.co.uk>
2021-04-30 09:53:32 +0100

    new changes

--- admin/admin.php
+++ admin/admin.php
@@ -4,7 +4,7 @@
 
 <main>
 
-<div class="fluid-container">
+<div class="container-fluid">
     <div class="row">
 
         <div class="col-sm">
@@ -13,12 +13,43 @@
             <a href="create.php"><button>Create Admin</button></a><br>
         </div>
     
-    <div class="col-sm"></div>
-    
-    <div class="col-sm"></div>
-    
+        <div class="col-sm">
+            <table style="background: black">
+                <tr style="background:#999">
+                    <th style="font-size: 15px; width:50px">ID:</th>
+                    <th style="font-size: 15px; width:250px">Bodyshop:</th>
+                    <th style="font-size: 15px; width:250px">Location:</th>
+                </tr>
+
+                <tr>
+                    <?php
+                    require'includes/dbh.inc.php';
+                        $sql = "SELECT * FROM bodyshop";
+                        $result = mysqli_query($conn, $sql);
+                        $resultCheck = mysqli_num_rows($result);
+
+                        if ($resultCheck > 0) {
+                            while ($row = mysqli_fetch_assoc($result)) {
+
+                            $bsid = $row['id'];
+
+                            echo
+                            '<tr>
+                            <td><a href="bsprofile.php?bsid='.$bsid.'"><button class="btn8 bsho">'.$row['id'].'</button></a></td>
+                            <td>'.$row['name'].'</td>
+                            <td>'.$row['town'].'</td></tr>';
+                            }
+                        }
+                    ?>
+                </tr>
+
+            </table>
+        </div>
+        
+        <div class="col-sm"></div>
+        
+        </div>
     </div>
-</div>
    
 
 </main>
